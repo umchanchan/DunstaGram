7295ff19b55e4b22e1ee7e3c96568d5815ec5274
umchanchan <umc@purdue.edu>
2024-07-09 14:04:20 -0400

    commit

--- src/Profile.java
+++ src/Profile.java
@@ -1,3 +1,6 @@
+import java.util.*;
+import java.io.*;
+
 /*
 This is the profile class of Dunstagram that has some information.
 
@@ -6,23 +9,41 @@ This is the profile class of Dunstagram that has some information.
 @author Mukund Rao
  */
 
+
+
+// login, signup, friend interaction, add or remove posts and comments
+//
 public class Profile {
     private String username;
     private String password;
     private int followers;
-    private int friends;
+    private int numFriends;
     private int age;
     private String gender;
+    private ArrayList<Profile> friends;
 
     public Profile(String username, String password, int age, String gender) {
         this.username = username;
         this.password = password;
         this.age = age;
         this.gender = gender;
+        this.friends = new ArrayList<>();
 
         this.followers = 0;
-        this.friends = 0;
+        this.numFriends = 0;
     }
+
+    public boolean signUp(String username, String password) {
+
+    }
+
+    public boolean login(String username, String password) {
+
+
+    }
+
+
+
 
 
 }
