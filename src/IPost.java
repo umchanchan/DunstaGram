977d892d23f2f1abbdbc7d9d3bdc9f8d30c923c9
mrao8 <raosmukund@gmail.com>
2024-07-09 21:02:56 -0400

    made interface for post and added "implements" inside of post

--- src/IPost.java
+++ src/IPost.java
@@ -1,2 +1,15 @@
 public interface IPost {
+    public String getMessage();
+    public void setMessage(String message);
+    public int getUpvotes();
+    public void setUpvotes(int upvotes);
+    public int getDownvotes();
+    public void setDownvotes(int downvotes);
+    public Profile getPoster();
+    public boolean isViewed();
+    public int getComments();
+    public String getCommentContent();
+    public void setCommentContent(String commentContent);
+    public void deleteComments();
+
 }
