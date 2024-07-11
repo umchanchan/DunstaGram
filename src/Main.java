5f3d6a79c7cf9541119b7b4cd8c943a2a8be3a42
mrao8 <raosmukund@gmail.com>
2024-07-11 14:02:39 -0400

    Made main method, feel free to use it

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+//This class is for testing purposes and we can change this from a textbased interface into a GUI later.
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
