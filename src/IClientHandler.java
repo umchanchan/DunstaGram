71e4d74474cd4b11bffc8b336d2030affd4726c8
Chan Um <umc@purdue.edu>
2024-07-15 20:00:53 -0400

    server and thread implemented

---
+++ src/IClientHandler.java
@@ -0,0 +1,2 @@
+public interface IClientHandler extends Runnable {
+}
---
+++ src/Server.java
@@ -0,0 +1,73 @@
+import java.io.*;
+import java.net.*;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ * This class mainly opens the server and prepares the server until the client connects
+ */
+public class Server implements IServer {
+
+    private boolean running = false;
+    private ServerSocket serverSocket;
+    private final int port;
+    private Base base;
+    private ExecutorService pool = Executors.newCachedThreadPool();
+
+    public Server(int port) {
+        this.base = null;
+        this.port = port;
+    }
+
+    public static void main(String[] args) {
+        Server server = new Server(12356); //port number not set
+
+        new Thread(server).start();
+    }
+
+
+    public void run() {
+        startServer();
+    }
+
+    //start the server method and when the user
+    public void startServer() {
+        try {
+            serverSocket = new ServerSocket(port);
+            running = true;
+            base = new Base();
+            base.readUserListFile();
+            base.readPostListFile();
+
+            while (running) {
+                Socket clientSocket = serverSocket.accept();
+                //every time a user connects, each user is going to be
+                //assigned to one clientHandler
+                pool.execute(new ClientHandler(clientSocket, base));
+
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (UserNotFoundException e) {
+            throw new RuntimeException(e);
+        } finally {
+            stopServer();
+        }
+    }
+
+    //stop the server method
+    public void stopServer() {
+        running = false;
+        pool.shutdown();
+        if (serverSocket != null) {
+            try {
+                serverSocket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+
+}
