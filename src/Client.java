1843dd479770f1f3dd21f069181e10e7b647d640
mrao8 <raosmukund@gmail.com>
2024-07-21 22:01:33 -0400

    changes

--- src/Client.java
+++ src/Client.java
@@ -14,39 +14,52 @@ public class Client implements IClient {
     public static void main(String[] args) throws UnknownHostException, IOException, ClassNotFoundException {
         Scanner sc = new Scanner(System.in); //to be deleted
         Socket clientSocket;
+        ObjectOutputStream oos;
+        ObjectInputStream ois;
         try {
             clientSocket = new Socket("localhost", 3588);
 
-            ObjectOutputStream oos = new ObjectOutputStream(clientSocket.getOutputStream());
-            ObjectInputStream ois = new ObjectInputStream(clientSocket.getInputStream());
+            oos = new ObjectOutputStream(clientSocket.getOutputStream());
+            ois = new ObjectInputStream(clientSocket.getInputStream());
 
             oos.flush();
-            oos.writeObject("makeComment");
-//            Profile chan = new Profile("Chan", "1123", 23, "Male");
-            Profile profile = new Profile("Chris", "11233");
-            Post post = new Post(profile, "Thank you");
-            oos.writeObject(post);
-            oos.writeObject("Thank you too");
-            oos.flush();
 
-            Object obj = ois.readObject();
-            if (obj instanceof String) {
-                System.out.println(obj);
-            } else if (obj instanceof Profile) {
-                System.out.println(((Profile) obj).getUsername());
-            } else if (obj instanceof Comment) {
-                System.out.println(((Comment) obj).getCommenter().getUsername());
-                System.out.println(((Comment) obj).getCommentContents());
-            }
-
-
-//        SwingUtilities.invokeLater(new SignUpGUI(clientSocket));//any new frame GUI
-            oos.close();
-            ois.close();
-            clientSocket.close();
-        } catch (IOException | ClassNotFoundException e) {
+        } catch (IOException e) {
             JOptionPane.showMessageDialog(null,
                     "Unable to connect to the server", "Error", JOptionPane.ERROR_MESSAGE);
+            return;
         }
+
+        SwingUtilities.invokeLater(new SignUpGUI(ois, oos));//any new frame GUI
+        String outcome = (String) ois.readObject();
+        System.out.println(outcome);
+
+
+
+
+        oos.close();
+        ois.close();
+        clientSocket.close();
+    }
+
+    public void test(ObjectInputStream ois, ObjectOutputStream oos) throws IOException, ClassNotFoundException {
+        oos.writeObject("makeComment");
+//            Profile chan = new Profile("Chan", "1123", 23, "Male");
+        Profile profile = new Profile("Chris", "11233");
+        Post post = new Post(profile, "Thank you");
+        oos.writeObject(post);
+        oos.writeObject("Thank you too");
+        oos.flush();
+
+        Object obj = ois.readObject();
+        if (obj instanceof String) {
+            System.out.println(obj);
+        } else if (obj instanceof Profile) {
+            System.out.println(((Profile) obj).getUsername());
+        } else if (obj instanceof Comment) {
+            System.out.println(((Comment) obj).getCommenter().getUsername());
+            System.out.println(((Comment) obj).getCommentContents());
+        }//moved testing code here (old phase 1 stuff)
+
     }
 }
